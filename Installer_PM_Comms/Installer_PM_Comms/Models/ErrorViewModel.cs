9206a66a58d9bea069bde885bef00e43785f44c5
Zach Mommaerts <zmommaerts@gmail.com>
2020-03-13 20:17:36 -0500

    initial

---
+++ Installer_PM_Comms/Installer_PM_Comms/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Installer_PM_Comms.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
