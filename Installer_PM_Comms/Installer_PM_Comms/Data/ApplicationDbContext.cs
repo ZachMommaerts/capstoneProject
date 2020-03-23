16abfd476c7f2f78ff540ccd1e7dbd621ad6c11e
Zach Mommaerts <zmommaerts@gmail.com>
2020-03-23 18:11:37 -0500

    seeded the database with clients/addresses/jobs to verify my views are pulling correctly

--- Installer_PM_Comms/Installer_PM_Comms/Data/ApplicationDbContext.cs
+++ Installer_PM_Comms/Installer_PM_Comms/Data/ApplicationDbContext.cs
@@ -24,6 +24,27 @@ namespace Installer_PM_Comms.Data
             builder.Entity<IdentityRole>().HasData(new IdentityRole { Name = "Admin", NormalizedName = "ADMIN" });
             builder.Entity<IdentityRole>().HasData(new IdentityRole { Name = "Project Manager", NormalizedName = "PROJECT MANAGER" });
             builder.Entity<IdentityRole>().HasData(new IdentityRole { Name = "Installer", NormalizedName = "INSTALLER" });
+
+            builder.Entity<Job>().HasData(
+                new Job { Id = 1, ClientId = 1, JobNumber = 247, JobName = "Restaurant Rail", Description = "new hand rail for downtown restaurant", InstallDate = DateTime.Parse("05-12-2020") },
+                new Job { Id = 2, ClientId = 2, JobNumber = 768, JobName = "Residential Fence", Description = "new residential fence in west allis", InstallDate = DateTime.Parse("04-10-2020") },
+                new Job { Id = 3, ClientId = 3, JobNumber = 134, JobName = "Stair Rail Remodel", Description = "remodel stair rail 12inches", InstallDate = DateTime.Parse("04-27-2020") },
+                new Job { Id = 4, ClientId = 4, JobNumber = 318, JobName = "Bar construction", Description = "new bar rail interior", InstallDate = DateTime.Parse("05-01-2020") }
+                );
+
+            builder.Entity<Client>().HasData(
+                new Client { Id = 1, AddressId = 1, CompanyName = "Hungry Sumo", ContactName = "jenny smith", ContactPhoneNumber = "569-2341", ContactEmailAddress = "jsmith@gmail.com" },
+                new Client { Id = 2, AddressId = 2, ContactName = "Rodney Herrington", ContactPhoneNumber = "567-3391", ContactEmailAddress = "rodh@gmail.com" },
+                new Client { Id = 3, AddressId = 3, CompanyName = "the blue circle", ContactName = "Jeff Griswold", ContactPhoneNumber = "490-5806", ContactEmailAddress = "bluecircle@gmail.com" },
+                new Client { Id = 4, AddressId = 4, CompanyName = "sports 'n drinks", ContactName = "rachel stamford", ContactPhoneNumber = "490-6317", ContactEmailAddress = "rachelstam@gmail.com" }
+                );
+
+            builder.Entity<Address>().HasData(
+               new Address { Id = 1, StreetName = "329 e pleasant st", City = "oconomowoc", State = "wi", ZipCode = 53066 },
+               new Address { Id = 2, StreetName = "2100 n kilian pl", City = "milwaukee", State = "wi", ZipCode = 53212 },
+               new Address { Id = 3, StreetName = "1427 s 75th st", City = "west allis", State = "wi", ZipCode = 53214 },
+               new Address { Id = 4, StreetName = "1105 57th st", City = "kenosha", State = "wi", ZipCode = 53140 }
+               );
         }
 
         public DbSet<Admin> Admins { get; set; }
